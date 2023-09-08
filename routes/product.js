a11a750d1f76390eaafdf7efa5fe0b5d2e602089
“VinayRinait” <vinaymasai1998@gmail.com>
2023-09-08 14:15:17 +0530

    banner_image

--- routes/product.js
+++ routes/product.js
@@ -5,7 +5,7 @@ const upload = require('../utils/productUpload');
 const auth = require("../config/middleware.js")
 
 router.post("/addProduct", productController.addProduct) 
-router.post("/getProduct", productController.getProduct)  
+router.get("/getProduct", productController.getProduct)  
 router.post("/getProductById", productController.getProductById) 
 router.post("/deleteProduct", productController.deleteProduct) 
 router.post("/updateProduct", productController.updateProduct)  
