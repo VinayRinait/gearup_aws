42fbcbc49ce1d9e3095760bce22a053215951e4b
“VinayRinait” <vinaymasai1998@gmail.com>
2023-09-08 16:49:25 +0530

    cors_error

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
+// Allow requests from http://localhost:3000
+app.use(cors({ origin: 'http://localhost:3000' }));
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 const path = require('path')
